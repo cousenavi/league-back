4a4adfb3460a3e0cd017718db2b220b7d06aef51
Fedor Avetisov <Fedor.Avetisov@alpari.org>
2014-09-13 11:58:17 +0400

    optimization done

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,8 @@ gulp.task('mobile-scripts', function() {
         .pipe(gulp.dest('public/mobile/compiled/'))
         .pipe(notify({ message: 'Scripts task complete' }));
 });
+
+
 gulp.task('admin-scripts', function() {
     return gulp.src('public/admin/coffee/*.coffee')
         .pipe(coffee())
@@ -28,7 +30,7 @@ gulp.task('admin-scripts', function() {
 
 
 gulp.task('mobile-scripts-concat', function() {
-    return gulp.src(['public/mobile/js/jquery-1.11.1.min.js', 'public/mobile/js/main.js'])
+    return gulp.src(['public/mobile/js/jquery.min.js', 'public/mobile/js/main.js'])
         .pipe(concat('main.js'))
         .pipe(gulp.dest('public/mobile/js/'))
         .pipe(uglify())
@@ -36,10 +38,24 @@ gulp.task('mobile-scripts-concat', function() {
         .pipe(notify({ message: 'Scripts task complete' }));
 });
 
+gulp.task('deploy-js', function() {
+   return gulp.src([
+       'public/lib/js/jquery.min.js',
+       'public/lib/js/bootstrap.min.js',
+       'public/admin/js/common.js'
+   ])
+       .pipe(concat('libs.js'))
+       .pipe(gulp.dest('public/js/'))
+});
+gulp.task('deploy-css', function() {
+   return gulp.src([
+       'public/lib/css/bootstrap.min.css',
+       'public/css/index.css'
+   ])
+       .pipe(concat('libs.min.css'))
+       .pipe(gulp.dest('public/css/'))
+});
 
-//gulp.task('default', ['clean'], function() {
-//    gulp.start('mobile-scripts');
-//});
 
 gulp.task('watch', function() {
     gulp.watch('public/mobile/coffee/*.coffee', ['mobile-scripts']);
