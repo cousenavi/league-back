956656e05417955d39a1c6c75654bff91280f593
favetisov <f.avetisov@gmail.com>
2014-11-05 22:11:27 +0400

    updated listplayers

--- public/blocks/listplayers/listplayers.js
+++ public/blocks/listplayers/listplayers.js
@@ -1,44 +1,33 @@
 (function() {
   (function($) {
-    var templates;
-    templates = {
-      body: function(sortOrder, players) {
-        var filteredPlayers, html, pl, sort, sortByFields, _i, _len;
-        sortByFields = function(fieldNames) {
-          return function(a, b) {
-            var fieldName, order;
-            for (fieldName in fieldNames) {
-              order = fieldNames[fieldName];
-              if (a[fieldName] < b[fieldName]) {
-                return 1 * order;
-              }
-              if (a[fieldName] > b[fieldName]) {
-                return -1 * order;
-              }
-            }
-            return 0;
-          };
-        };
-        sort = {};
-        sort[sortOrder] = 1;
-        sort['played'] = -1;
-        filteredPlayers = players.sort(sortByFields(sort)).filter(function(pl) {
-          return pl[sortOrder] > 0;
-        });
-        html = '';
-        for (_i = 0, _len = filteredPlayers.length; _i < _len; _i++) {
-          pl = filteredPlayers[_i];
-          html += "                <tr>\n                  <td>\n" + (pl.teamLogo ? "<img style='width: 20px; height: 20px' src='/" + pl.teamLogo + "'>&nbsp;" : "") + pl.name + "</td>\n                  <td " + (sortOrder === 'goals' ? "class='active'" : '') + "> " + pl.goals + "</td>\n                  <td " + (sortOrder === 'assists' ? "class='active'" : '') + ">" + pl.assists + "</td>\n                  <td " + (sortOrder === 'points' ? "class='active'" : '') + ">" + pl.points + "</td>\n                  <td " + (sortOrder === 'yellow' ? "class='active'" : '') + ">" + pl.yellow + "</td>\n                  <td " + (sortOrder === 'red' ? "class='active'" : '') + ">" + pl.red + "</td>\n                  <td " + (sortOrder === 'played' ? "class='active'" : '') + ">" + pl.played + "</td>\n                </tr>";
+    var sortByField, templates;
+    sortByField = function(fieldName) {
+      return function(a, b) {
+        if (a[fieldName] < b[fieldName]) {
+          return 1;
         }
-        return html;
+        if (a[fieldName] > b[fieldName]) {
+          return -1;
+        }
+        return 0;
+      };
+    };
+    templates = {
+      player: function(pl, field) {
+        return "<tr>\n  <td><img src=\"/" + pl.teamLogo + "\"> " + pl.name + "</td>\n  <td>" + pl[field] + "</td>\n</tr>";
       },
-      table: function(sortOrder, body) {
-        var arrow, html;
-        arrow = "&nbsp;<span class='glyphicon glyphicon-arrow-down arrow' style='color: green'></span>";
-        html = "<table id=\"topplayers\" class=\"table table-striped\">\n<thead>\n  <th>Name</th>\n  <th data-sort=\"goals\">G" + (sortOrder === 'goals' ? arrow : '') + "</th>\n  <th data-sort=\"assists\">A" + (sortOrder === 'assists' ? arrow : '') + "</th>\n  <th data-sort=\"points\">G+A" + (sortOrder === 'points' ? arrow : '') + "</th>\n\n  <th data-sort=\"yellow\"><span class='glyphicon glyphicon-book' style=\"color:yellow\"></span>\n    " + (sortOrder === 'yellow' ? arrow : '') + "</th>\n  <th data-sort=\"red\"><span class='glyphicon glyphicon-book' style=\"color:red\"></span>\n    " + (sortOrder === 'red' ? arrow : '') + "</th>\n  <th data-sort=\"played\">GP" + (sortOrder === 'played' ? arrow : '') + "</th>\n</thead><tbody>";
-        html += body;
-        html += '</tbody></table>';
-        return html;
+      table: function(players, fieldName, leagueName) {
+        var html, pl, pls;
+        pls = ((function() {
+          var _i, _len, _results;
+          _results = [];
+          for (_i = 0, _len = players.length; _i < _len; _i++) {
+            pl = players[_i];
+            _results.push(templates.player(pl, fieldName));
+          }
+          return _results;
+        })()).join('');
+        return html = "<div id=\"prv\">\n  <div id='head'>\n      <div id='leagueName'>" + leagueName + "</div><div id='tourNumber'>" + fieldName + "</div>\n  </div>\n  <table>\n    " + pls + "\n  </table>\n</div>";
       }
     };
     return $.fn.listplayers = function(leagueId, field) {
@@ -46,9 +35,12 @@
         leagueId: leagueId
       }, (function(_this) {
         return function(table) {
-          var body;
-          body = templates.body(field, table.players);
-          return _this.html(templates.table(field, body));
+          var LIMIT, filteredPlayers;
+          LIMIT = 15;
+          filteredPlayers = table.players.sort(sortByField(field)).filter(function(pl) {
+            return pl[field] > 0 && pl[field] >= table.players[LIMIT][field];
+          });
+          return _this.html(templates.table(filteredPlayers, field, 'Amateur Portugal League'));
         };
       })(this));
     };
